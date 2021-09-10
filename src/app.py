d929034d612687b017fe418f57c2ce346ffd1cb1
IsaiahAim <halmitonoluwatobi@gmail.com>
2021-09-10 13:05:46 +0100

    Update User interface and record on Linux OS

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,Response,request,stream_with_context
+from flask import Flask,render_template,Response,request
 import cv2
 import os
 import datetime as datetime,time
@@ -11,23 +11,22 @@ switch =0
 frame =None
 
 
-
 def record(out):
     global rec_frame, frame
     rec_frame =frame
     while True:
-
         time.sleep(0.05)
-        out.write(frame)
+        out.write(rec_frame)
+
 
 def getAviNameWithDate(nameIn="output_video.avi"):
     """Needs a file ending on .avi, inserts _<date> before .avi. """
-    
+
     if not nameIn.endswith(".avi"):
         raise ValueError("filename must end on .avi")
-
     filename = nameIn.replace(".avi","_{0}.avi").format(datetime.datetime.now().strftime("%Y-%m-%d"))
     return filename
+
 
 def generate_frames():
     global rec_frame, camera,frame
@@ -35,19 +34,14 @@ def generate_frames():
         pass
     else:
         while True:
-                
-            ## read the camera frame
             success,frame=camera.read()
             if not success:
                 break
             else:
                 rec_frame=frame
-                #frame= cv2.putText(cv2.flip(frame,1),"Recording...", (0,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),4)
-                #frame=cv2.flip(frame,1)
+                frame=cv2.flip(frame,1)
                 ret,buffer=cv2.imencode('.jpg',frame)
                 frames=buffer.tobytes()
-                
-
 
             yield(b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + frames + b'\r\n')
@@ -65,18 +59,18 @@ def video():
 def tasks():
     global switch,camera, out,thread,start_time, end_time,frame
     if request.method == 'POST':
-        
         if request.form.get('start') == 'Start':
             start_time =datetime.datetime.now()
             print(start_time)
             camera = cv2.VideoCapture(0)
-            fourcc = cv2.VideoWriter_fourcc(*'XVID')
-            out = cv2.VideoWriter("output.avi", fourcc, 20.0, (640, 480))
+            # Get the width and height of frame
+            width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
+            height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+            file_path=os.path.normpath(os.getcwd() + os.sep + os.pardir+'/video/output.mp4')
+            out = cv2.VideoWriter(file_path, fourcc, 20.0, (width, height))
             thread = Thread(target = record, args=[out,])
-            thread.start()  #Start new thread for recording the video
-            print(request.form['textarea'])
-
-            
+            thread.start()  #Start new thread for recording the video  
         else:
             pass
         if request.form.get('stop') == 'Stop':
@@ -86,21 +80,13 @@ def tasks():
             duration = divmod(minutes_dif.seconds,60)
             minute,second=duration[0],duration[1]
             print(duration)
-            camera.release()
             out.release()
+            camera.release()
             cv2.destroyAllWindows()
         else: pass
-        
-
-        
     elif request.method=='GET':
         return render_template('index.html')
-    return render_template('index.html',test =request.form['textarea'])
-
-
-  
-
-    
+    return render_template('index.html',test ='testing')
 
 
 if __name__ == '__main__':
