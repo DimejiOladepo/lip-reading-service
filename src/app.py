e464baa6c3c3d1201742d76ad413143a87bb8f29
Dimeji Oladepo <dimeji1603@gmail.com>
2021-09-15 16:29:32 +0100

    mac changes

--- src/app.py
+++ src/app.py
@@ -1,31 +1,18 @@
-from flask import Flask,render_template,Response,request
+from flask import Flask, render_template, Response, request
 import cv2
 import os
-import datetime as datetime,time
+import datetime as datetime, time
 from threading import Thread
 import jyserver.Flask as jsf
-from init import db,app,directory
+from init import db,app, directory
 from models import camera_task, operation
 from audio import text_to_speech
 
+
 global camera,switch, rec_frame,start_time,end_time,frame
 camera = None
-switch =0
-frame =None
+frame = None
 
-'''
-def folder_creat(video_name="output.mp4" ):
-    name="video"
-    directory=os.path.normpath(os.getcwd() + os.sep + os.pardir)
-    os.chdir(directory)
-    file_path = os.listdir()
-    if name in file_path:
-        return os.path.join(directory,name,video_name)
-    else:
-        os.mkdir(name)
-        print(f'Folder "{name}" succesfully created!')
-        return os.path.join(directory,name,video_name)
-'''
 
 def record(out):
     global rec_frame, frame
@@ -58,16 +45,13 @@ def generate_frames():
 class App:
     def __init__(self):
         
-        self.startTime= 0
-        self.stopTime =0
-        self.textStartTime=0
-        self.savingTime=0
+        self.startTime = 0
+        self.stopTime = 0
+        self.textStartTime = 0
+        self.savingTime = 0
+
     def getTime(self):
         self.startTime =datetime.datetime.now()
-        print('time:', self.startTime)
-        
-
-        #self.js.document.getElementById("time").innerHTML=20
         
     def endTime(self):
         self.stopTime =datetime.datetime.now()
@@ -80,18 +64,16 @@ class App:
         self.savingTime=datetime.datetime.now()
         print('savingTime:', self.savingTime)
 
+
 @app.route('/')
 def index():
     if not os.path.exists(os.path.join(directory,'database' ,'data.db')):
         db.create_all()
     return App.render(render_template('index.html'))
     
-
-
 @app.route('/video')
 def video():
     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
-
 
 @app.route('/requests',methods=['POST','GET'])
 def tasks():
@@ -99,39 +81,46 @@ def tasks():
     if request.method == 'POST':
         user_input = request.form['text']
         if request.form.get('start') == 'Start':
-            start_time =datetime.datetime.now()
-            print(start_time)
-            camera = cv2.VideoCapture(0)
-            # Get the width and height of frame
-            width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
-            height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
-            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-            video_file =os.path.join(directory,'video' ,'output.mp4')
-            out = cv2.VideoWriter(video_file, fourcc, 20.0, (width, height))
-            thread = Thread(target = record, args=[out,])
-            thread.start()  #Start new thread for recording the video  
-        else:
-            pass
+            if camera !=None:
+                pass
+            else:
+                start_time =datetime.datetime.now()
+                print(start_time)
+                camera = cv2.VideoCapture(0)
+                # Get the width and height of frame
+                width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
+                height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+                fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+                video_file =os.path.join(directory,'video' ,'output.mp4')
+                out = cv2.VideoWriter(video_file, fourcc, 20.0, (width, height))
+                thread = Thread(target = record, args=[out,])
+                thread.start()  #Start new thread for recording the video  
+        
         if request.form.get('stop') == 'Stop':
-            camera.release()
-            out.release()
-            cv2.destroyAllWindows()
-        else: pass
+            if camera == None:
+                pass
+            else:
+                camera.release()
+                out.release()
+                cv2.destroyAllWindows()
+                camera = None
+       
+        if request.form.get('audio') == 'Audio':
+            text_to_speech(user_input)
+       
         if request.form.get('save') == 'Save':
            
             cam_duration=App.stopTime-App.startTime
-            cam_duration=format(cam_duration.total_seconds(),".2f")
+            cam_duration=format(cam_duration.total_seconds(),'.2f')
             session_duration =App.savingTime-App.textStartTime
-            session_duration =format(session_duration.total_seconds(), ".2f")
+            session_duration =format(session_duration.total_seconds(), '.2f')
             table1_input=camera_task(App.startTime,App.stopTime,cam_duration)
             table2_input =operation(App.textStartTime,App.savingTime,session_duration,user_input)
             db.session.add(table1_input)
             db.session.add(table2_input)
             db.session.commit()
 
-        if request.form.get('audio') =="Audio":
-            print('audio')
-            text_to_speech(user_input)
+        
 
            
     elif request.method=='GET':
--- src/audio.py
+++ src/audio.py
@@ -1,7 +1,7 @@
 import pyttsx3
 
 
-def text_to_speech(text, gender='Female'):
+def text_to_speech(text, gender='Male'):
     """
     Function to convert text to speech
     :param text: text
@@ -11,7 +11,7 @@ def text_to_speech(text, gender='Female'):
     voice_dict = {'Male': 0, 'Female': 1}
     code = voice_dict[gender]
 
-    engine = pyttsx3.init()
+    engine = pyttsx3.init('nsss')
 
     # Setting up voice rate
     engine.setProperty('rate', 120)
@@ -22,8 +22,7 @@ def text_to_speech(text, gender='Female'):
     # Change voices: 0 for male and 1 for female
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[code].id)
-    try:
-        engine.say(text)
-        engine.runAndWait()
-    except RuntimeError as e:
-        pass
+  
+    engine.say(text)
+    engine.runAndWait()
+
